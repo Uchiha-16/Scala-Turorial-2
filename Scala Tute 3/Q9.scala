a1e067ebb04d343b9b58984a9efc73d683b6e380
Varsha Wijethunge <varrave16@gmail.com>
2022-07-14 17:08:19 +0530

    Update Q9.scala

--- Scala Tute 3/Q9.scala
+++ Scala Tute 3/Q9.scala
@@ -1,17 +1,8 @@
-object Celsius_Fahrenheit{
 
-  def main(args: Array[String]) : Unit = {
+import java.util.Scanner;
 
-    var Celcius: Int = 37
-    var Fahrenheit: Int = 98
-    println(CtoF(Celcius));
-    println(FtoC(Fahrenheit));
-  }
-
-  def CtoF(temp: Int): Double = {
-      return((temp * 9.0 / 5.0) + 32.0) ;
-  }
-  def FtoC(temp: Int): Double = {
-    return(((temp-32) * 5) / 9);
-  }
+object CALCULATOR{
+    def main(args: Array[String]): Unit = {
+        
+    }
 }
